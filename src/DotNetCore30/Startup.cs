71d043ef72244cf528014363ba0abbb853a9301b
Rob Lankey <roblankey@quickenloans.com>
2019-10-17 11:28:40 -0400

    add swashbuckle / swagger

--- src/DotNetCore30/Startup.cs
+++ src/DotNetCore30/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using DotNetCore30.HealthChecks;
 using Serilog;
 
@@ -12,8 +13,14 @@ namespace DotNetCore30
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
             services.AddHealthChecks()
                 .AddCheck<BasicHealthCheck>("basic_health_check");;
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Example API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -26,6 +33,12 @@ namespace DotNetCore30
             app.UseSerilogRequestLogging();
 
             app.UseHttpsRedirection();
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Example API v1");
+            });
 
             app.UseRouting();
 
